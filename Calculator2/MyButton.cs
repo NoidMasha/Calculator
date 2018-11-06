f97cd22e37a5a5f0c683d8468252df3b20b934de
Admin <Admin@IKAP-770001391>
2018-11-06 10:39:34 +0330

    Add project files.

---
+++ Calculator2/MyButton.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator2
+{
+    class MyButton:System.Windows.Forms.Button
+    {
+        public MyButton():base()
+        {
+
+        }
+        public MyButton(Calculator MyCalculatorPointer):base()
+        {
+            this.Click += new System.EventHandler(OnButtonClick);
+            myCalculatorPointer = MyCalculatorPointer;
+        }
+
+        private static string buttonText;
+        public static string ButtonText
+        {
+            get
+            {
+                return buttonText;
+            }
+        }
+
+        public Calculator myCalculatorPointer;
+        private void OnButtonClick(object sender, EventArgs e)
+        {
+            MyButton button = (MyButton)sender;
+            buttonText = button.Text;
+            myCalculatorPointer.UpdateOnClick();
+        }
+
+    }
+}
---
+++ Calculator2/MyLabel.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator2
+{
+    class MyLabel : System.Windows.Forms.Label
+    {
+        public MyLabel() : base()
+        {
+
+        }
+
+        public string InputText
+        {
+            set
+            {
+                if (value != null)
+                {
+                    int lenght = value.Length;
+                    if (!value.Contains("."))
+                    {
+                        for (int i = 1; i * 3 < lenght; i++)
+                        {
+                            value = value.Insert(lenght - 3 * i, ",");
+                        }
+                    }
+                    else
+                    {
+                        int pointIndex = value.IndexOf(".");
+                        for (int i = 1; i * 3 < pointIndex; i++)
+                        {
+                            value = value.Insert(pointIndex - 3 * i, ",");
+                        }
+
+                    }
+                    Text = value;
+                }
+            }
+        }
+    }
+}
